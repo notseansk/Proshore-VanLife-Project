7ba2bd3afdb3c4edfc82e98d45ef23c91635aaeb
Shaun Pant <crizone.shaun@gmail.com>
2023-11-30 02:17:36 +0545

    dynamic vans page updated

--- src/components/Vans.tsx
+++ src/components/Vans.tsx
@@ -26,15 +26,21 @@ const Vans = () => {
     <div className="vans container m-auto">
       <h1 className="font-bold text-4xl m-10 ">Explore our van option</h1>
       <div className="list-vans flex flex-row flex-wrap gap-5 items-center justify-center">
-        {vansList.map((one: VansType) => (
-          <VansList
-            key={one.id}
-            name={one.name}
-            price={one.price}
-            imageUrl={one.imageUrl}
-            type={one.type}
-          />
-        ))}
+        {vansList.length > 0 ? (
+          vansList.map((one: VansType) => (
+            <VansList
+              key={one.id}
+              name={one.name}
+              price={one.price}
+              imageUrl={one.imageUrl}
+              type={one.type}
+            />
+          ))
+        ) : (
+          <h1 className="text-4xl my-[180px] font-black text-[#e17654]">
+            Loading...
+          </h1>
+        )}
       </div>
     </div>
   );
